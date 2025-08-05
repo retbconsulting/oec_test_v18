d54b8ff89822e13e3391844c03ea3463bb1e4cc5
Mohammed OUCHANI <ouchani.mohammed1@gmail.com>
2025-08-05 13:16:06 +0100

    [RESET] : Reset code source

--- mandat_cotisation_management/models/base/__init__.py
+++
@@ -1,3 +0,0 @@
-# from . import res_users
-from . import res_partner
-from . import cotisation_csv
--- mandat_cotisation_management/models/base/res_users.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, api 
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    @api.constrains('partner_id')
-    def _check_value(self):
-        if self.partner_id.company_type == 'company':
-            raise ValidationError("Vous ne pouvez pas créer un utilisateur pour une société")
